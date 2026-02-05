dcb682798124be2a4cea21b1bca12cac03bc6576
Dhanush18100 <dhanush.nayak.100@gmail.com>
2026-02-05 06:44:11 +0530

    commit

---
+++ Day1/countDigits.java
@@ -0,0 +1,12 @@
+public class countDigits {
+    public static void main(String[] args) {
+        int n=123;
+        int count=0;
+        while(n!=0){
+           // n=n%10;
+            n/=10;
+            count++;
+        }
+        System.out.println(count);
+    }
+}
