d4d696ca6a327ad76ac324e60c37bda5ee8078a3
Dhanush18100 <dhanush.nayak.100@gmail.com>
2026-02-05 07:13:38 +0530

    Searching Alogrithms

---
+++ Day1/BinarySearch.java
@@ -0,0 +1,25 @@
+public class BinarySearch {
+    public static void main(String[] args) {
+         int[] arr = {2, 5, 8, 12, 16, 23, 38};
+        int target = 16;
+        System.out.println(binarySearch(arr, target));
+        
+    }
+    public static int binarySearch(int []arr,int target){
+        int low=0;
+        int high=arr.length-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+
+            if(arr[mid]==target){
+                return mid;
+            }
+            else if(arr[mid]<target){
+                low=mid+1;
+            }else{
+                high=mid-1;
+            }
+        }
+        return -1;
+    }
+}
